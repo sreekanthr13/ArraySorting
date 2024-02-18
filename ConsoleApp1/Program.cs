eb3cb02e16088702ecfffccfcc31b8ec78200b6f
sreekanthr13 <38903062+sreekanthr13@users.noreply.github.com>
2024-02-18 18:42:28 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[] { 2, 9, 4, 3, 5, 1, 7 };
+            int temp = 0;
+
+            for (int i = 0; i <= intArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    if (intArray[i] > intArray[j])
+                    {
+                        temp = intArray[i];
+                        intArray[i] = intArray[j];
+                        intArray[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Array sort in asscending order");
+            foreach (var item in intArray)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
